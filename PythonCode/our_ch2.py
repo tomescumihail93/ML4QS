5e548e6f3ec9f25d8580206a0052f50030f3b08f
Tomescu Mihail <tomescumihail93@gmail.com>
2018-06-05 17:31:45 +0200

    Added script for our dataset, problem with plotting - not appearing properly

---
+++ PythonCode/our_ch2.py
@@ -0,0 +1,100 @@
+##############################################################
+#                                                            #
+#    Mark Hoogendoorn and Burkhardt Funk (2017)              #
+#    Machine Learning for the Quantified Self                #
+#    Springer                                                #
+#    Chapter 2                                               #
+#                                                            #
+##############################################################
+
+
+dataset_path = '../our_dataset/'
+result_dataset_path = './intermediate_datafiles/'
+
+# Import the relevant classes.
+
+from Chapter2.CreateDataset import CreateDataset
+from util.VisualizeDataset import VisualizeDataset
+from util import util
+import copy
+import os
+
+
+if not os.path.exists(result_dataset_path):
+    print('Creating result directory: ' + result_dataset_path)
+    os.makedirs(result_dataset_path)
+
+# Chapter 2: Initial exploration of the dataset.
+
+# Set a granularity (i.e. how big are our discrete time steps). We start very
+# coarse grained, namely one measurement per minute, and secondly use four measurements
+# per second
+
+granularities = [60000, 250]
+datasets = []
+
+for milliseconds_per_instance in granularities:
+
+    # Create an initial dataset object with the base directory for our data and a granularity
+    DataSet = CreateDataset(dataset_path, milliseconds_per_instance)
+
+    # Add the selected measurements to it.
+
+    # We add the accelerometer data (continuous numerical measurements) of the phone and the smartwatch
+    # and aggregate the values per timestep by averaging the values/
+    DataSet.add_numerical_dataset('our_dataset_ns.csv', 'accelerometerTimestamp_sinceReboot(s)', ['accelerometerAccelerationX(G)','accelerometerAccelerationY(G)','accelerometerAccelerationZ(G)'], 'avg', 'acc_phone_')
+
+    # Line for smartwatch data, unavailable
+    # DataSet.add_numerical_dataset('accelerometer_smartwatch.csv', 'timestamps', ['x','y','z'], 'avg', 'acc_watch_')
+
+    # We add the gyroscope data (continuous numerical measurements) of the phone and the smartwatch
+    # and aggregate the values per timestep by averaging the values/
+    DataSet.add_numerical_dataset('our_dataset_ns.csv', 'gyroTimestamp_sinceReboot(s)', ['gyroRotationX(rad/s)','gyroRotationY(rad/s)','gyroRotationZ(rad/s)'], 'avg', 'gyr_phone_')
+    
+    # DataSet.add_numerical_dataset('gyroscope_smartwatch.csv', 'timestamps', ['x','y','z'], 'avg', 'gyr_watch_')
+
+    # We add the heart rate (continuous numerical measurements) and aggregate by averaging again
+    # DataSet.add_numerical_dataset('heart_rate_smartwatch.csv', 'timestamps', ['rate'], 'avg', 'hr_watch_')
+
+    # We add the labels provided by the users. These are categorical events that might overlap. We add them
+    # as binary attributes (i.e. add a one to the attribute representing the specific value for the label if it
+    # occurs within an interval).
+    # DataSet.add_event_dataset('our_dataset_ns.csv', 'label_start', 'label_end', 'activity(txt)', 'binary')
+
+    # We add the amount of light sensed by the phone (continuous numerical measurements) and aggregate by averaging again
+    # amount of light sensed is missing from our dataset
+    # DataSet.add_numerical_dataset('light_phone.csv', 'timestamps', ['lux'], 'avg', 'light_phone_')
+
+    # We add the magnetometer data (continuous numerical measurements) of the phone and the smartwatch
+    # and aggregate the values per timestep by averaging the values
+    DataSet.add_numerical_dataset('our_dataset_ns.csv', 'motionTimestamp_sinceReboot(s)', ['motionMagneticFieldX(uT)','motionMagneticFieldY(uT)','motionMagneticFieldZ(uT)'], 'avg', 'mag_phone_')
+    #DataSet.add_numerical_dataset('magnetometer_smartwatch.csv', 'timestamps', ['x','y','z'], 'avg', 'mag_watch_')
+
+    # We add the pressure sensed by the phone (continuous numerical measurements) and aggregate by averaging again
+    DataSet.add_numerical_dataset('our_dataset_ns.csv', 'activityTimestamp_sinceReboot(s)', ['pedometerNumberofSteps(N)'], 'avg', 'pedometer_phone_')
+
+    # Get the resulting pandas data table
+
+    dataset = DataSet.data_table
+
+    # Plot the data
+
+    DataViz = VisualizeDataset()
+
+    # Boxplot
+    DataViz.plot_dataset_boxplot(dataset, ['acc_phone_accelerometerAccelerationX(G)','acc_phone_accelerometerAccelerationY(G)','acc_phone_accelerometerAccelerationZ(G)'])
+
+    # Plot all data
+    DataViz.plot_dataset(dataset, ['acc_', 'gyr_', 'mag_', 'pedometer_phone_'], ['like', 'like', 'like', 'like', 'like', 'like', 'like','like'], ['line', 'line', 'line', 'line', 'line', 'line', 'points', 'points'])
+
+    # And print a summary of the dataset
+
+    util.print_statistics(dataset)
+    datasets.append(copy.deepcopy(dataset))
+
+# And print the table that has been included in the book
+
+util.print_latex_table_statistics_two_datasets(datasets[0], datasets[1])
+
+# Finally, store the last dataset we have generated (250 ms).
+dataset.to_csv(result_dataset_path + 'chapter2_result.csv')
